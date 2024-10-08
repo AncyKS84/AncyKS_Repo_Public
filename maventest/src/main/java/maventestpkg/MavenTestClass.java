e11cb6ce4f2bdb9b1e774b8ad84f2e9c9bd65fc6
Ancy K S <ancyksunny@gmail.com>
2024-10-08 20:41:18 +0530

    Maven Test

---
+++ maventest/src/main/java/maventestpkg/MavenTestClass.java
@@ -0,0 +1,15 @@
+package maventestpkg;
+
+public class MavenTestClass {
+	public void display()
+	{
+		System.out.println("Message");
+	}
+
+	public static void main(String[] args) {
+		
+		MavenTestClass ob=new MavenTestClass();
+		ob.display();
+	}
+
+}
